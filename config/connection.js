af7833ef5da6ecce96556c4a75820dffa076b17d
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-12 00:20:36 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 
 let connection;
 
-if(process.env.burger_db){
-connection = mysql.createConnection(process.env.burger_db);
+if(process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
  }else{connection = mysql.createConnection({
   host: "localhost",
   port: 3306,

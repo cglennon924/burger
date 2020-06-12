f5b86d1a99973aaecb3e62b89886a76d3de1a0b8
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-12 15:11:42 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-USE burger_db;
+INSERT INTO burgers(devoured,burger_name) VALUES(true, 'Whopper');
+INSERT INTO burgers(devoured,burger_name) VALUES(true,'Big Mac');
+INSERT INTO burgers(devoured,burger_name) VALUES(false,'Steak Burger');
 
-INSERT INTO burgers(name)
-VALUES ("Whopper"), ("Big Mac"), ("Steak Burger"), ("Mugshot Burger");
 
